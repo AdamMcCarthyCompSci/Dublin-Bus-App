2d635bb1b695e91e9a15f54a1324189c18f16d9f
Turlough-Cooke <72612458+Turlough-Cooke@users.noreply.github.com>
2021-06-23 15:19:11 +0100

    Removed sensitive data to config file.

--- GTFS_Realtime.py
+++ GTFS_Realtime.py
@@ -1,29 +1,27 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64
+import urllib.request, urllib.parse, urllib.error
 import pandas as pd
 import json
+import numpy as np
+import config
+from sqlalchemy import create_engine
 
 headers = {
     # Request headers
     'Cache-Control': 'no-cache',
-    'x-api-key': '1e368d52ec3c4abe9229bc3df32f207d',
+    'x-api-key': config.GTFS_R_API_Key,
 }
 
 params = urllib.parse.urlencode({
 })
 
-# Production = https://gtfsr.transportforireland.ie/v1/?format=json
-# testing = https://api.nationaltransport.ie/gtfsrtest/?format=json
-url = 'https://api.nationaltransport.ie/gtfsrtest/?format=json'
-req = urllib.request.Request(url, None, headers)
+req = urllib.request.Request(config.GTFS_url, None, headers)
 
 # Create the response that will be parsed
 with urllib.request.urlopen(req) as f:
     response = f.read()
 
-my_bytes_value = response
-
 # to double quotes to make it valid JSON
-my_json = my_bytes_value.decode('utf8').replace("'", '"')
+my_json = response.decode('utf8').replace("'", '"')
 
 # Load the JSON to a Python list & dump it back out as formatted JSON
 data = json.loads(my_json)
@@ -67,4 +65,26 @@ df_final = pd.concat([df, arrival['arrival_delay'], arrival['arrival_time'],
                       departure['departure_delay'], departure['departure_time']],
                      axis=1)
 df_final.drop(['departure', 'arrival'], axis=1, inplace=True)
+df_final = df_final.loc[:, ~df_final.columns.duplicated()]
 
+df_final = df_final.reset_index(drop=True)
+df_final['id'] = df_final.index
+# shift column 'Name' to first position
+first_column = df_final.pop('id')
+
+# insert column using insert(position, column_name, first_column) function
+df_final.insert(0, 'id', first_column)
+
+data = df_final
+
+# Split data into smaller sets (only useful for very large sets)
+split_data = np.array_split(data, 1)
+
+# Create engine
+engine = create_engine(
+    'mysql+mysqlconnector://admin:DublinBus123.@dublinbus.ccfxbrwhc5i2.us-east-1.rds.amazonaws.com:3306/dublinbus',
+    echo=False)
+
+for dataset in split_data:
+    dataset.to_sql(name='tfi_realtime', con=engine, if_exists='replace', index=False)
+    print('finished with set')
