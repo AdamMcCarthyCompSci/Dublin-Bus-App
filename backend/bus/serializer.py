04641836152eba5cb655e07420fe35fcde8c2d3d
Maria Leech <maria.leech@ucdconnect.ie>
2021-07-07 16:56:31 +0100

    update to the serializer.py file to add the dublin bus routes fields

--- backend/bus/serializer.py
+++ backend/bus/serializer.py
@@ -3,5 +3,8 @@ from . models import *
 
 class ReactSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = Results
-		fields = ['title', 'directions', 'prediction']
+		model = TfiStops
+		fields = ['stop_id', 'stop_name', 'stop_lat', 'stop_lon']
+
+		model2 = DublinBusRoutes
+		field2 = ["dublin_bus_routes_id", "routename","routedescription","direction", "platecode","shortcommonname_en"]
