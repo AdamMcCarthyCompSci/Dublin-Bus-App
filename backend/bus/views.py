b38e5e2e97e08ec5000f061d6754d8556df51435
Maria Leech <maria.leech@ucdconnect.ie>
2021-07-07 16:57:27 +0100

    update to the views.py file to add the dublin bus routes view

--- backend/bus/views.py
+++ backend/bus/views.py
@@ -5,18 +5,19 @@ from rest_framework.response import Response
 from . serializer import *
 # Create your views here.
 
-class ResultsView(APIView):
-	
-	serializer_class = ReactSerializer
+
+class StopsView(APIView):
 
 	def get(self, request):
-		results = [ {"title": result.title,"directions": result.directions,"prediction": result.prediction}
-		for result in Results.objects.all()]
-		return Response(results)
+		stops = [{"id": stop.stop_id, "name": stop.stop_name, "stop_lat": stop.stop_lat, "stop_lon": stop.stop_lon}
+		for stop in TfiStops.objects.all()]
+		return Response(stops)
 
-	def post(self, request):
+class Routes(APIView):
+	def get(self, request):
+		routes = [{"id": route.dublin_bus_routes_id, "busnumber": route.routename,"routedescription":route.routedescription, "direction": route.direction, "platecode": route.platecode,"shortcommonname_en":route.shortcommonname_en}
+		for route in DublinBusRoutes.objects.all()]
+		return Response(routes)
 
-		serializer = ReactSerializer(data=request.data)
-		if serializer.is_valid(raise_exception=True):
-			serializer.save()
-			return Response(serializer.data)
+
+
